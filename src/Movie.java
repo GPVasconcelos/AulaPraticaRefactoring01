36fd2329a94437904873aa49483556d59c44917a
GPVasconcelos <166875237+GPVasconcelos@users.noreply.github.com>
2024-11-26 21:42:34 -0300

    Commit 11: Herança

--- src/Movie.java
+++ src/Movie.java
@@ -1,54 +1,49 @@
 public class Movie {
 
-  public static final int  CHILDRENS = 2;
-  public static final int  REGULAR = 0;
-  public static final int  NEW_RELEASE = 1;
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
 
-  private String _title;
-  private int _priceCode;
+    private String _title;
+    private Price _price;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
-  }
+    public Movie(String title, int priceCode) {
+        _title = title;
+        setPriceCode(priceCode);
+    }
 
-  public int getPriceCode() {
-      return _priceCode;
-  }
+    public int getPriceCode() {
+        return _price.getPriceCode();
+    }
 
-  public void setPriceCode(int arg) {
-      _priceCode = arg;
-  }
-
-  public String getTitle (){
-      return _title;
-  }
-
-    public double getCharge(int daysRented) {
-        double thisAmount = 0;
-        switch (_priceCode) {
+    public void setPriceCode(int arg) {
+        switch (arg) {
             case REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                thisAmount += daysRented * 3;
+                _price = new RegularPrice();
                 break;
             case CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
+                _price = new ChildrensPrice();
                 break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid price code");
         }
-        return thisAmount;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public double getCharge(int daysRented) {
+        return _price.getPriceCode(); // Por enquanto, isso permanece
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((_priceCode == NEW_RELEASE) && daysRented > 1) {
+        if ((getPriceCode() == NEW_RELEASE) && daysRented > 1) {
             return 2;
         }
         return 1;
     }
-
 }
