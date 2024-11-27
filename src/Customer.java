53442dc7e962044224193b06c17fec222743142e
GPVasconcelos <166875237+GPVasconcelos@users.noreply.github.com>
2024-11-26 21:00:57 -0300

    Commit 6: Extract and Move Method

--- src/Customer.java
+++ src/Customer.java
@@ -26,23 +26,20 @@ public class Customer {
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
 
-         // Chamada direta ao método getCharge
+
          result += "\t" + each.getMovie().getTitle() + "\t" +
                  String.valueOf(each.getCharge()) + "\n";
          totalAmount += each.getCharge();
 
-         // add frequent renter points
-         frequentRenterPoints++;
-         if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                 each.getDaysRented() > 1) frequentRenterPoints++;
+
+         frequentRenterPoints += each.getFrequentRenterPoints();
       }
 
-      // add footer lines
+
       result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
       result += "You earned " + String.valueOf(frequentRenterPoints) +
               " frequent renter points";
       return result;
    }
-
 
 }
